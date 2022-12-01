510ff910404f80e1b3e808ca10bf8ed7322996e3
sizak <sizak.js@gmail.com>
2022-12-01 21:45:49 +0900

    v0.001

---
+++ lib/component/calendar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800),
+      lastDay: DateTime(3000),
+      headerStyle: HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+          titleTextStyle: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 16
+          )
+      ),
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay){
+        print(selectedDay);
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+    );
+  }
+}
